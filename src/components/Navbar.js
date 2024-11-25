23d2a3e3417392848f4eeeb0c5056beb76ec8846
lindavataksi <lindavataksi3@gmail.com>
2024-11-24 23:53:31 -0500

    Responsive Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import { FaSearch } from "react-icons/fa";
 import { HiOutlineShoppingBag } from "react-icons/hi";
+import { TiThMenu } from "react-icons/ti";
 import './styles/Navbar.css'
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
   return (
     <div className='navbar-container '>
       <Link to="/" className='navbar-container-logo' >Stylehub.</Link>
-      <div className='navbar-container-links'>
+      <div className={`navbar-container-links ${isMenuOpen ? 'active' : ''}`}>
         <Link to="/" >Home</Link>
         <Link to="/" >Shop</Link>
         <Link to="/"> Product</Link>
@@ -16,7 +22,9 @@ const Navbar = () => {
       <div className = 'navbar-container-icons'>
         < FaSearch />
         < HiOutlineShoppingBag />
+        <div className='navbar-container-menu-icon' onClick={toggleMenu}> <TiThMenu /></div>
       </div>
+
     </div>
   )
 }
